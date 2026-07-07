8aa07562ea12958ff70007e35584ebb632df0041
skyinertia <skyinertia.0@gmail.com>
2026-07-07 10:33:36 +0700

    configure image & added image.nvim as dependency in neo-tree.lua

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,13 +2,14 @@ return {
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
-		
+
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-      			"MunifTanjim/nui.nvim",
-      			"nvim-tree/nvim-web-devicons",
-    		},
-    		
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"3rd/image.nvim",
+		},
+
 		lazy = false,
-  	},
+	},
 }
