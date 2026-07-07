9a79fe50f74c732385339e672c0d7d163754d0d7
skyinertia <skyinertia.0@gmail.com>
2026-07-07 23:29:29 +0700

    migrated nvim-treesitter to neovim-treesitter in plugins/treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,36 +1,43 @@
 return {
 	{
-		"nvim-treesitter/nvim-treesitter",
-		branch = "main",
+		"neovim-treesitter/nvim-treesitter",
 		lazy = false,
 		build = ":TSUpdate",
-
-		opts = {
-			ensure_installed = {
-				"lua",
-				"python",
-				"bash",
-				"vim",
-				"vimdoc",
-				"query",
-				"cpp",
-				"json",
-				"yaml",
-				"toml",
-				"markdown",
-				"markdown_inline",
-				-- add more here when needed
-			},
-
-			auto_install = true,
-
-			highlight = {
-				enabled = true,
-			},
-
-			indent = {
-				enabled = true,
-			},
+		dependencies = {
+			"neovim-treesitter/treesitter-parser-registry",
 		},
+
+		config = function()
+			require("nvim-treesitter").setup()
+
+			local langs = {
+				lua = true,
+				python = true,
+				bash = true,
+				vim = true,
+				vimdoc = true,
+				query = true,
+				cpp = true,
+				json = true,
+				yaml = true,
+				toml = true,
+				markdown = true,
+			}
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local ft = vim.bo[args.buf].filetype
+					if not langs[ft] then
+						return
+					end
+
+					local lang = vim.treesitter.language.get_lang(ft) or ft
+					if vim.treesitter.language.add(lang) then
+						vim.treesitter.start(args.buf, lang)
+						vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
+			})
+		end,
 	},
 }
