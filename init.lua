fc8f613b0c53928a6bcac8fa2d54cbdbb252aa48
skyinertia <skyinertia.0@gmail.com>
2026-07-07 12:10:22 +0700

    created keymaps/others.lua, migrated keymaps from init.lua to others.lua, & added others.lua reference to keymaps/init.lua

--- init.lua
+++ init.lua
@@ -4,42 +4,8 @@ vim.g.maplocalleader = "\\"
 
 -- Requires
 require("config.lazy")
+require("keymaps.init")
 require("colors.caelestia-dynamic-colors").setup()
-
--- Custom Mappings
-vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
-
--- remove arrow keys cuz trash
---[[vim.keymap.set({ "n", "i", "v" }, "<Up>", "<Nop>")
-vim.keymap.set({ "n", "i", "v" }, "<Down>", "<Nop>")
-vim.keymap.set({ "n", "i", "v" }, "<Left>", "<Nop>")
-vim.keymap.set({ "n", "i", "v" }, "<Right>", "<Nop>")]]
-
-vim.keymap.set("n", "<leader>tl", function()
-	local wo = vim.wo
-
-	if wo.number and not wo.relativenumber then
-		-- absolute -> relative
-		wo.number = false
-		wo.relativenumber = true
-		print("Line Numbering: Relative")
-	elseif not wo.number and wo.relativenumber then
-		-- relative -> absolute + relative
-		wo.number = true
-		wo.relativenumber = true
-		print("Line Numbering: Absolute + Relative")
-	else
-		-- absolute + relative -> absolute
-		wo.number = true
-		wo.relativenumber = false
-		print("Line Numbering: Absolute")
-	end
-end, { desc = "Cycle line number modes" })
-
-vim.keymap.set("n", "<leader>tw", function()
-	vim.wo.wrap = not vim.wo.wrap
-	print("Wrap: " .. (vim.wo.wrap and "ON" or "OFF"))
-end, { desc = "Toggle line wrap" })
 
 -- Other Settings
 vim.opt.number = true
