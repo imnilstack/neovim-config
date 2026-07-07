0023cbddf82e42f0fc3f2caee8c71e89fe4895a6
skyinertia <skyinertia.0@gmail.com>
2026-07-07 12:51:50 +0700

    added keymapping for closing current buffer in keymaps/bufferline.lua

--- lua/keymaps/bufferline.lua
+++ lua/keymaps/bufferline.lua
@@ -2,3 +2,4 @@ local map = vim.keymap.set
 
 map("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
 map("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+map("n", "<leader>bd", ":bprevious | bdelete #<CR>", { desc = "Close current buffer" }, { silent = true })
