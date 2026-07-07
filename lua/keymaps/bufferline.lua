e673949287117c5c4501cd874b153c49a8469476
skyinertia <skyinertia.0@gmail.com>
2026-07-07 12:47:53 +0700

    created keymaps/bufferline.lua, added keymappings, & added file reference to keymaps/init.lua

---
+++ lua/keymaps/bufferline.lua
@@ -0,0 +1,4 @@
+local map = vim.keymap.set
+
+map("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+map("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
