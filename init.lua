9e501ea4307f3c76ad212696aa372a6d182713f9
skyinertia <skyinertia.0@gmail.com>
2026-07-07 12:13:51 +0700

    migrated colorscheme watcher from init.lua to config/autocmds.lua

--- init.lua
+++ init.lua
@@ -5,31 +5,6 @@ vim.g.maplocalleader = "\\"
 -- Requires
 require("config.lazy")
 require("config.options")
+require("config.autocmds")
 require("keymaps.init")
 require("colors.caelestia-dynamic-colors").setup()
-
--- Colorscheme Watcher
-local dir = vim.fn.expand("~/.config/hypr/scheme")
-local watcher = vim.uv.new_fs_event()
-local timer = vim.uv.new_timer()
-
-watcher:start(
-	dir,
-	{},
-	vim.schedule_wrap(function(err, filename)
-		if err or filename ~= "current.conf" then
-			return
-		end
-
-		timer:stop()
-		timer:start(
-			100,
-			0,
-			vim.schedule_wrap(function()
-				package.loaded["colors.caelestia-dynamic-colors"] = nil
-				require("colors.caelestia-dynamic-colors").setup()
-				vim.cmd("redraw!")
-			end)
-		)
-	end)
-)
---
+++ lua/config/autocmds.lua
@@ -0,0 +1,25 @@
+-- Colorscheme Watcher
+local dir = vim.fn.expand("~/.config/hypr/scheme")
+local watcher = vim.uv.new_fs_event()
+local timer = vim.uv.new_timer()
+
+watcher:start(
+	dir,
+	{},
+	vim.schedule_wrap(function(err, filename)
+		if err or filename ~= "current.conf" then
+			return
+		end
+
+		timer:stop()
+		timer:start(
+			100,
+			0,
+			vim.schedule_wrap(function()
+				package.loaded["colors.caelestia-dynamic-colors"] = nil
+				require("colors.caelestia-dynamic-colors").setup()
+				vim.cmd("redraw!")
+			end)
+		)
+	end)
+)
